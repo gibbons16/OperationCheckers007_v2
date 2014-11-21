d51618e73346252671947b518b9fdf3299ac3cc8
orangeback <brianthorpe50@yahoo.com>
2014-11-20 23:20:14 -0500

    Good enough login for now

--- 007Checkers/src/CheckersClient007/LoginScreen.java
+++ 007Checkers/src/CheckersClient007/LoginScreen.java
@@ -1,55 +1,95 @@
 package CheckersClient007;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
+import javax.swing.JTextField;
 
 public class LoginScreen extends JPanel {
-	/**
-	 * @wbp.nonvisual location=358,214
-	 */
-	private final ImageIcon imageIcon = new ImageIcon();
+
+	private  ImageIcon imageIcon = new ImageIcon("src\\Images\\black.jpg");
 	private JButton btnConnect;
-	private JTextArea userNameTextArea;
-	private JTextArea ipAddress;
+	private JTextField userNameTestField;
+	private JTextField ipAddressTestField;
+	private JLabel imageLabel;
 
 	/**
 	 * Create the panel.
 	 */
 	public LoginScreen() {
-		setLayout(null);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
 		
-		 userNameTextArea = new JTextArea();
-		userNameTextArea.setBounds(195, 400, 405, 32);
-		add(userNameTextArea);
+	   imageLabel = new JLabel(imageIcon);
 		
-		 ipAddress = new JTextArea("130.108.28.165"); // derek's IP
-		ipAddress.setBounds(195, 445, 405, 32);
-		add(ipAddress);
-		
-		JLabel lblNewLabel = new JLabel("IP");
-		lblNewLabel.setBounds(167, 459, 56, 16);
-		add(lblNewLabel);
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.gridheight = 4;
+		gbc_panel.insets = new Insets(0, 0, 5, 0);
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 1;
+		gbc_panel.gridy = 1;
+		add(imageLabel, gbc_panel);
 		
 		JLabel lblUsername = new JLabel("Username");
-		lblUsername.setBounds(128, 413, 78, 16);
-		add(lblUsername);
+		GridBagConstraints gbc_lblUsername = new GridBagConstraints();
+		gbc_lblUsername.insets = new Insets(0, 0, 5, 5);
+		gbc_lblUsername.anchor = GridBagConstraints.EAST;
+		gbc_lblUsername.gridx = 0;
+		gbc_lblUsername.gridy = 6;
+		add(lblUsername, gbc_lblUsername);
+		
+		userNameTestField = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 1;
+		gbc_textField_1.gridy = 6;
+		add(userNameTestField, gbc_textField_1);
+		userNameTestField.setColumns(10);
+		
+		JLabel lblIp = new JLabel("IP");
+		GridBagConstraints gbc_lblIp = new GridBagConstraints();
+		gbc_lblIp.insets = new Insets(0, 0, 5, 5);
+		gbc_lblIp.anchor = GridBagConstraints.EAST;
+		gbc_lblIp.gridx = 0;
+		gbc_lblIp.gridy = 7;
+		add(lblIp, gbc_lblIp);
+		
+		ipAddressTestField = new JTextField("130.108.28.165");
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 0);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 7;
+		add(ipAddressTestField, gbc_textField);
+		ipAddressTestField.setColumns(10);
 		
 		 btnConnect = new JButton("Connect");
-		btnConnect.setBounds(299, 532, 97, 25);
-		add(btnConnect);
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.gridx = 1;
+		gbc_btnNewButton.gridy = 8;
+		add(btnConnect, gbc_btnNewButton);
+
 
 	}
 	public String getUserName()
 	{
 		
-		return userNameTextArea.getText().toString();
+		return userNameTestField.getText().toString();
 	}
 	public String getIpAddress()
 	{
-		return ipAddress.getText().toString();
+		return ipAddressTestField.getText().toString();
 	}
 	public JButton getConnectButton()
 	{
